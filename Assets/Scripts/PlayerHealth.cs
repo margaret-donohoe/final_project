85abb1058065497f9e94622372bbc70dd134a7f9
Ish <igowrish@pratt.edu>
2024-04-18 12:05:21 -0400

    PlayerHealth

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxHealth = 100f;
+    public float playerHealth;
+    public Image playerHealthBar;
+    //private Animator playerAnimator;
+
+    private void Start()
     {
-        
+        playerHealth = maxHealth;
+        //playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayerDamage(float damage)
     {
-        
+        playerHealth -= damage;
+        playerHealthBar.fillAmount = playerHealth / 100f;
+        if (playerHealth <= 0)
+        {
+            Debug.Log("You Died.");
+        }
     }
+
 }
+
+
